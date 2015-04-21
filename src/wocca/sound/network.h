51dd4b27c7f2ab705a0a4ef69ad30f01a5a54c62
Mike <mike@mikeseymour.co.uk>
2015-04-21 17:50:39 +0100

    Tidying up sound network

--- src/wocca/sound/network.h
+++ src/wocca/sound/network.h
@@ -18,14 +18,15 @@ struct connect<input<T1,I1>, output<T2,I2>> : connect<output<T2,I2>, input<T1,I1
 
 template <typename T1, unsigned I1, typename T2, unsigned I2>
 struct connect<output<T1,I1>, input<T2,I2>> {
-    typedef magic::graph::edge<T1, T2> edge_type;
+    typedef magic::graph::edge<T1, T2> edge;
 };
 
 template <typename... Connections> class network {
 public:
     void process() {magic::tuple::visit(components, process_helper());}
 
-    typename magic::graph::sort<typename Connections::edge_type...>::result components;
+private:
+    typename magic::graph::sort<typename Connections::edge...>::result components;
 
     // C++14: use a generic lambda [](auto && c){c.process();}
     struct process_helper {template <typename Component> void operator()(Component && c) {c.process();}};
