bc9328bdf3a57c17beb4b433c9d9b35e1f3b5e2a
Mike Seymour <mike@mikeseymour.co.uk>
2022-02-02 16:40:23 +0000

    Types for tracking stream positions and offsets

---
+++ src/test/unit/choir/position.cpp
@@ -0,0 +1,40 @@
+#include "wocca/choir/position.h"
+#include "test/catch.hpp"
+
+#include <iostream>
+
+using pos = wocca::choir::pos8;
+using off = wocca::choir::off8;
+
+TEST_CASE("wocca/choir/position", "Position/offset arithmetic"){
+  CHECK(pos{2} + off{3} == pos{5});
+  CHECK(off{3} + pos{2} == pos{5});
+  CHECK(pos{5} - off{3} == pos{2});
+  CHECK(pos{5} - pos{3} == off{2});
+  CHECK(pos{3} - pos{5} == off{-2});
+
+  CHECK(pos{0xfe} + off{5} == pos{0x03});
+  CHECK(pos{0x03} - off{5} == pos{0xfe});
+  CHECK(pos{0x03} - pos{0xfe} == off{5});
+
+  CHECK(-off{3} == off{-3});
+
+  CHECK(pos{0} < pos{1});
+  CHECK(pos{1} > pos{0});
+  CHECK_FALSE(pos{1} < pos{0});
+  CHECK_FALSE(pos{0} > pos{1});
+  CHECK_FALSE(pos{1} < pos{1});
+  CHECK_FALSE(pos{1} > pos{1});
+
+  CHECK(pos{0} <= pos{1});
+  CHECK(pos{1} <= pos{1});
+  CHECK(pos{1} >= pos{0});
+  CHECK(pos{1} >= pos{1});
+  CHECK_FALSE(pos{1} <= pos{0});
+  CHECK_FALSE(pos{0} >= pos{1});
+
+  CHECK(pos{0x7f} < pos{0x80});
+  CHECK(pos{0x80} < pos{0x81});
+  CHECK(pos{0xff} < pos{0x00});
+}
+
---
+++ src/wocca/choir/position.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <type_traits>
+#include <cstdint>
+
+namespace wocca {
+namespace choir {
+
+// Representation of positions and offsets within a finite section of a
+// potentially infinite stream. The types are affine: positions can be
+// subtracted to give an offset, which can be applied to other offsets.
+//
+// For an N-bit offset type, arithmetic and comparisons are valid as long as
+// the absolute distance between positions is less than 2^(N-1).
+template <typename U, typename S = std::make_signed_t<U>>
+struct position_details
+{
+  struct pos {U val;};
+  struct off {S val;};
+
+  friend pos operator+(pos x, off y) {return {U(x.val + y.val)};}
+  friend pos operator+(off x, pos y) {return {U(x.val + y.val)};}
+  friend pos operator-(pos x, off y) {return {U(x.val - y.val)};}
+
+  friend off operator-(pos x, pos y) {return {S(x.val - y.val)};}
+
+  friend off operator+(off x, off y) {return {S(x.val + y.val)};}
+  friend off operator-(off x, off y) {return {S(x.val - y.val)};}
+  friend off operator-(off x)        {return {S(-x.val)};}
+
+  friend bool operator==(pos x, pos y) {return x.val == y.val;}
+  friend bool operator!=(pos x, pos y) {return x.val != y.val;}
+
+  friend bool operator==(off x, off y) {return x.val == y.val;}
+  friend bool operator!=(off x, off y) {return x.val != y.val;}
+
+  friend bool operator< (pos x, pos y) {return (x - y).val <  0;}
+  friend bool operator<=(pos x, pos y) {return (x - y).val <= 0;}
+  friend bool operator> (pos x, pos y) {return (x - y).val >  0;}
+  friend bool operator>=(pos x, pos y) {return (x - y).val >= 0;}
+
+  template <typename Stream>
+  friend Stream& operator<<(Stream& s, pos x) {return s << +x.val;}
+  template <typename Stream>
+  friend Stream& operator<<(Stream& s, off x) {return s << +x.val;}
+};
+
+using pos64 = position_details<uint64_t>::pos;
+using off64 = position_details<uint64_t>::off;
+
+using pos32 = position_details<uint32_t>::pos;
+using off32 = position_details<uint32_t>::off;
+
+using pos16 = position_details<uint16_t>::pos;
+using off16 = position_details<uint16_t>::off;
+
+using pos8 = position_details<uint8_t>::pos;
+using off8 = position_details<uint8_t>::off;
+
+}
+}
+
